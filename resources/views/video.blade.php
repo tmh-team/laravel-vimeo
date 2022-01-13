d655169cd7908487cb04d1342793648ee4aad0d4
Thet Minn Htun <thetminnhtun92@gmail.com>
2022-01-13 09:15:39 +0630

    Implement vimeo player

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <title>Vimeo Video</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
     <div class="container mx-auto">
         <div class="lg:flex lg:space-x-10 lg:p-10">
@@ -15,28 +17,33 @@
                     @if($errors->any())
                     <ul>
                         @foreach ($errors->all() as $error)
-                            <li class="bg-red-200 p-2 my-2">{{ $error }}</li>
+                        <li class="bg-red-200 p-2 my-2">{{ $error }}</li>
                         @endforeach
                     </ul>
                     @endif
                     <form action="/" method="post" enctype="multipart/form-data">
                         @csrf
                         <input type="text" name="title" placeholder="Title" class="border w-full p-3 mt-5">
-            
+
                         <textarea name="description" placeholder="Description" class="border w-full p-3 mt-5"></textarea>
-            
+
                         <input type="file" name="video" class="mt-5 w-full">
-            
+
                         <button type="submit" class="mt-5 bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded shadow">Upload</button>
                     </form>
                 </div>
             </div>
             <div class="lg:w-1/2 space-y-5">
                 @foreach ($videos as $video)
-                    <div class="border p-5">{{ $video->vimeo_video_id }}</div>
+                <div class="border p-5 h-[400px]">
+                    <iframe src="https://player.vimeo.com/video/{{ $video->vimeo_video_id }}" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </div>
                 @endforeach
             </div>
         </div>
     </div>
+
+    <script src="https://player.vimeo.com/api/player.js"></script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,25 @@
 <?php
 
 use App\Models\Video;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Vimeo\Laravel\VimeoManager;
 
 Route::get('/', function () {
     return view('video', [
-        'videos' => []
+        'videos' => Video::all()
     ]);
 });
 
-Route::post('/', function (Request $request, VimeoManager $vimeo) {
+Route::post('/', function (VimeoManager $vimeo) {
     request()->validate([
         'title' => 'required',
         'description' => 'required',
         'video' => 'required|mimetypes:video/mp4,video/avi,video/mpeg,video/quicktime',
     ]);
 
-    $path =  $vimeo->upload(request()->file('video'));
+    $path = $vimeo->upload(request()->file('video'));
+    $path = explode('/', $path);
+    $path = end($path);
     
     Video::create([
         'title' => request('title'),
